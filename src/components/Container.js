e70ed3bd9cb5e97d140db9b8f881846b151f5b15
Kris Necula <kristen.necula@ppfa.org>
2018-12-31 10:53:49 -0500

    Added locations list

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,16 +7,25 @@ import * as LocationsAPI from "../api/Locations";
 //for our componentDidMount we're importing our promise as LocationsAPI
 //to use in our components.
 
+//store the API data in a state and hook it to our imported LocationsAPI promise
+//to keep the data dynamic and pass it throughout our components.
+//ex: this.state.locations has been passed as a prop to List component and Map component.
+
 class Container extends Component {
+  state = {
+    locations: []
+  }
   componentDidMount() {
-    LocationsAPI.getLocations().then(resp => console.log("RESP", resp));
+    LocationsAPI.getLocations().then(resp =>
+      this.setState({ locations: resp })
+    );
   }
   render () {
-    console.log("RESPONSE");
+    console.log("DATA");
     return (
       <div className="container">
-        <List />
-        <Map />
+        <List locations={this.state.locations} />
+        <Map locations={this.state.locations} />
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import Map from './Map';
 
 class List extends Component {
   render () {
     //return filtered location list
+    const locations = this.props.locations;
     return (
-      <div id="list" aria-label="filered list">
-        <h2>List</h2>
+      <div id="list" aria-label="locations list">
+        <h2>Locations</h2>
+        <input id="search"
+          placeholder="Search for..."
+          type="text"
+          aria-label="search"
+          />
+
+          <button>Search</button>
+
+          <ol>
+            {locations.map(loc => (
+              <li key={loc.venue.id}>
+                <div>
+                  <p><strong>{loc.venue.name}</strong></p>
+                  <p>Address: {loc.venue.location.formattedAddress[0]}</p>
+                </div>
+              </li>
+            ))}
+          </ol>
       </div>
-    )
+    );
   }
-}
+};
 
   export default List;
